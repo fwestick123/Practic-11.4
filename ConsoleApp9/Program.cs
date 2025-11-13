7bcf3e320de96995133666727d86b3a6f4acc822
fwestick123 <galcov2509@gmail.com>
2025-11-13 08:32:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player Andrey = new Player(5, 100);
+            Andrey.TakeDamage(30);
+            Andrey.TakeDamage(20);
+            Andrey.TakeDamage(40);
+            Andrey.TakeDamage(10);
+        }
+    }
+}
